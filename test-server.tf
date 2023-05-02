8502eb3bb35a11a4e7a7dbf1a6c8c6a82fc6c113
vlad <sidyakin.vlad@gmail.com>
2023-05-02 11:38:09 +0300

    Update test-server.tf

--- test-server.tf
+++ test-server.tf
@@ -1,13 +1,12 @@
 ## Create a server
-resource "hcloud_server" "gravitsapa" {
-  name        = "git.vlad.linkpc.net"
+resource "hcloud_server" "test-server01" {
+  name        = "test01.vlad.linkpc.net"
   image       = "debian-11"
   server_type = "${var.tiny}"
   backups     = "false"
   placement_group_id = hcloud_placement_group.placement-group.id
   datacenter  = "fsn1-dc14"
   ssh_keys = data.hcloud_ssh_keys.all_keys.ssh_keys.*.name
-  #user_data = file("docker_install.sh")
   user_data = file("cloud01.yaml")
   public_net {
     ipv4_enabled = true
@@ -23,16 +22,16 @@ resource "hcloud_server" "gravitsapa" {
 }
 
 # Network attachment
-resource "hcloud_server_network" "srvnetwork" {
-  server_id  = hcloud_server.gravitsapa.id
+resource "hcloud_server_network" "test-server01" {
+  server_id  = hcloud_server.test-server01.id
   network_id = data.terraform_remote_state.network.outputs.network_ids["Falkenstein"].id
 }
 
 ## Placement Group
 resource "hcloud_placement_group" "placement-group" {
-  name = "gravitsapa"
+  name = "test-server01"
   type = "spread"
   labels = {
-    "name" : "gravitsapa"
+    "name" : "test-server01"
   }
 }
